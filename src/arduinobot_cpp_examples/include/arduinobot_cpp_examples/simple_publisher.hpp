3f5a49cac5e55fd04276ea176f5e01e92346ca6a
ellrussbest <ellrussbest@gmail.com>
2024-12-04 01:24:35 +0300

    Added pragma to the header

--- src/arduinobot_cpp_examples/include/arduinobot_cpp_examples/simple_publisher.hpp
+++ src/arduinobot_cpp_examples/include/arduinobot_cpp_examples/simple_publisher.hpp
@@ -1,19 +1,23 @@
+#ifndef SIMPLE_PUBLISHER_HPP_
+#define SIMPLE_PUBLISHER_HPP_
+
 #include <chrono>
+#include <functional>
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
-#include <functional>
 
 using namespace std::chrono_literals;
 
-namespace my{
+namespace my {
 class simple_publisher : public rclcpp::Node {
    public:
     simple_publisher(std::string&& node_name, std::string&& topic_name,
                      const unsigned int& buffer_size)
-        : Node(node_name), counter(0)
-    {
-        publisher = create_publisher<std_msgs::msg::String>(topic_name, buffer_size);
-        timer = create_wall_timer(1s, std::bind(&simple_publisher::callback, this));
+        : Node(node_name), counter(0) {
+        publisher =
+            create_publisher<std_msgs::msg::String>(topic_name, buffer_size);
+        timer =
+            create_wall_timer(1s, std::bind(&simple_publisher::callback, this));
         RCLCPP_INFO(get_logger(), "Publishing at 1 Hz");
     }
 
@@ -30,4 +34,5 @@ void simple_publisher::callback() {
     msg.data = "Hello ROS 2 - counter: " + std::to_string(counter++);
     publisher->publish(msg);
 }
-}
+}  // namespace my
+#endif  // SIMPLE_PUBLISHER_HPP_
